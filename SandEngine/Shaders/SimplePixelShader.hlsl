cde0fc162d771b14427c5e7eb2fefdbfcb063d90
etri3600 <wndrksgud@gmail.com>
2017-04-19 08:10:22 +0900

    simple shader dummy

--- SandEngine/Shaders/SimplePixelShader.hlsl
+++ SandEngine/Shaders/SimplePixelShader.hlsl
@@ -1,4 +1,17 @@
-float4 main(float4 pos : SV_POSITION) : SV_TARGET
+struct PixelShaderInput
 {
-	return 0;
+	float4 fragPos : SV_POSITION;
+	float3 PosW : POSITION0;
+	float4 color : COLOR0;
+	float3 NormalW : NORMAL0;
+	float3 TangentW : TANGENT0;
+	float2 uv : TEXCOORD0;
+};
+
+Texture2D<half4> diff : register(t0);
+SamplerState samp : register(s0);
+
+float4 main(PixelShaderInput input) : SV_TARGET
+{
+	return diff.Sample(samp, input.uv);
 }
--- SandEngine/Shaders/SimpleVertexShader.hlsl
+++ SandEngine/Shaders/SimpleVertexShader.hlsl
@@ -1,4 +1,46 @@
-float4 main(float4 pos : POSITION) : SV_POSITION
+struct ViewProjection
 {
-	return 0;
+	column_major matrix model;
+	column_major matrix view;
+	column_major matrix projection;
+	column_major matrix normalMatrix;
+};
+
+ConstantBuffer<ViewProjection> ViewProjectionConstantBuffer : register(b0);
+
+struct VertexShaderInput
+{
+	float3 pos : POSITION;
+	float4 color : COLOR0;
+	float3 normal : NORMAL0;
+	float4 tangent : TANGENT0;
+	float2 uv : TEXCOORD0;
+};
+
+struct PixelShaderInput
+{
+	float4 fragPos : SV_POSITION;
+	float3 PosW : POSITION0;
+	float4 color : COLOR0;
+	float3 NormalW : NORMAL0;
+	float3 TangentW : TANGENT0;
+	float2 uv : TEXCOORD0;
+};
+
+PixelShaderInput main(VertexShaderInput input)
+{
+	PixelShaderInput output;
+
+	float4 posW = mul(ViewProjectionConstantBuffer.model, float4(input.pos, 1.0f));
+	float4 pos = mul(ViewProjectionConstantBuffer.view, posW);
+	pos = mul(ViewProjectionConstantBuffer.projection, pos);
+
+	output.fragPos = pos;
+	output.PosW = posW.xyz;
+	output.color = input.color;
+	output.NormalW = mul(ViewProjectionConstantBuffer.normalMatrix, input.normal).xyz;
+	output.TangentW = mul(ViewProjectionConstantBuffer.normalMatrix, input.tangent).xyz;
+	output.uv = input.uv;
+
+	return output;
 }
